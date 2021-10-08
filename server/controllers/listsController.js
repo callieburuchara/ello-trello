ab3191528d14e0e9adbd15366faf8772997a3b1a
Callie <48188690+callieburuchara@users.noreply.github.com>
2021-10-08 12:17:13 -0400

    Both backend stories completed for List Management epic

--- server/controllers/listsController.js
+++ server/controllers/listsController.js
@@ -6,28 +6,62 @@ const { validationResult } = require("express-validator");
 const createList = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
+
     const objToSend = {
       boardId: req.body.boardId,
-      title: req.body.list.title
+      title: req.body.list.title,
+      position: 65535,
+      cards: []
     }
     List.create(objToSend)
       .then((list) => {
-        List.find({ _id: list._id }, "title _id boardId createdAt updatedAt").then(
+        List.findById(list._id, "title _id boardId createdAt updatedAt").then(
           (list) => {
-            list = list[0]
-            console.log(list.boardId)
-            Board.updateOne({_id: list.boardId }, {$addToSet: {lists:list}})
-            return res.json({ list })
+            req.list = list
+            next()
           }
         );
       })
       .catch((err) => 
-      console.error(err)
-        // next(new HttpError("Creating list failed, please try again", 500))
+        next(new HttpError("Creating list failed, please try again", 500))
       );
   } else {
     return next(new HttpError("The input field is empty.", 404));
   }
 };
 
+const addListToBoard = (req, res, next) => {
+  const list = req.list;
+  const boardId = req.body.boardId;
+
+  Board.findByIdAndUpdate(boardId, {
+    $addToSet: { lists: list._id }, 
+  }).then(() => {
+    next();
+  });
+};
+
+const updateListTitle = (req, res, next) => {
+  const listId = req.params.id
+  const title = req.body.title
+
+  console.log(title)
+
+  List.findByIdAndUpdate(listId, { title })
+    .then((list) => {
+      req.list = list
+      next()
+    }
+  ) 
+}
+
+const sendList = (req, res, next) => {
+  res.json(req.list)
+}
+
 exports.createList = createList;
+exports.addListToBoard = addListToBoard
+exports.updateListTitle = updateListTitle
+exports.sendList = sendList
+
+
--- server/routes/api.js
+++ server/routes/api.js
@@ -10,6 +10,8 @@ router.post('/boards', validateBoard, boardsController.createBoard );
 
 router.get('/boards/:id', boardsController.getBoard)
 
-router.post('/lists', listsController.createList)
+router.post('/lists', listsController.createList, listsController.addListToBoard, listsController.sendList)
+
+router.put('/lists/:id', listsController.updateListTitle, listsController.sendList)
  
 module.exports = router;
