75e235dc5b9ab2f280ae2170d8a58ad9b86349a3
Callie <48188690+callieburuchara@users.noreply.github.com>
2021-10-12 11:52:59 -0400

    Backend of Card Management done — post & get card

--- server/controllers/cardsController.js
+++ server/controllers/cardsController.js
@@ -1,40 +1,73 @@
 const List = require("../models/lists")
 const Card = require("../models/card")
+const Board = require("../models/board")
 const HttpError = require("../models/httpError");
 const { validationResult } = require("express-validator");
+
+const getCard = (req, res, next) => {
+  const id = req.params.id
+
+  Card.findOne({ _id: id })
+    // populate comments here
+  .then((card) => res.json(card))
+  .catch((err) => next(new HttpError("There is no such card.", 404)))
+}
 
 const createCard = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
-    // We need to format the object to send with the right properties.
+    const listId = req.body.listId
+
+    let boardId;
+    List.findById(listId, "boardId").then(list => boardId = list.boardId )
+
+
     const objToSend = {
       "title": req.body.card.title,
       "description": "",
       "labels": [],
-      "listId": req.body.listId,
+      "listId": listId,
       "position": 65535.0,
       "archived": false,
       "dueDate": null,
       "completed": false,
-      "boardId": 1,
+      "boardId": boardId,
       "comments": [],
-      "actions": []
-      "commentsCount": 0
+      "actions": [], 
+      "commentsCount": 0 
     }
 
-    List.create(objToSend)
-      .then((list) => {
-        List.findById(list._id, "title _id boardId createdAt updatedAt").then(
-          (list) => {
-            req.list = list
+    Card.create(objToSend)
+      .then((card) => {
+        Card.findById(card._id, "title _id listId boardId createdAt updatedAt").then(
+          (card) => {
+            req.card = card
             next()
           }
         );
       })
       .catch((err) => 
-        next(new HttpError("Creating list failed, please try again", 500))
+        next(new HttpError("Creating card failed, please try again", 500))
       );
   } else {
     return next(new HttpError("The input field is empty.", 404));
   }
 };
+
+const addCardToList = (req, res, next) => {
+  const card = req.card
+  const listId = card.listId
+
+  List.findByIdAndUpdate(listId, {
+    $addToSet: { cards: card._id },
+  }).then(() => next() )
+};
+
+const sendCard = (req, res, next) => {
+  res.json(req.card)
+}
+
+exports.createCard = createCard;
+exports.addCardToList = addCardToList;
+exports.sendCard = sendCard
+exports.getCard = getCard
--- server/routes/api.js
+++ server/routes/api.js
@@ -5,16 +5,21 @@ const boardsController = require("../controllers/boardsController");
 const cardsController = require("../controllers/cardsController")
 const { validateBoard } = require("../validators/validators");
 
+// BOARDS
 router.get('/boards',boardsController.getBoards );
 
 router.post('/boards', validateBoard, boardsController.createBoard );
 
 router.get('/boards/:id', boardsController.getBoard)
 
+// LISTS
 router.post('/lists', listsController.createList, listsController.addListToBoard, listsController.sendList)
 
 router.put('/lists/:id', listsController.updateListTitle, listsController.sendList)
 
+
+// CARDS
+router.get('/cards/:id', cardsController.getCard)
 router.post('/cards', cardsController.createCard, cardsController.addCardToList, cardsController.sendCard)
  
 module.exports = router;
